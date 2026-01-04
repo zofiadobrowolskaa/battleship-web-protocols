f80cfd243786fae197cf31513967e96f22493c6f
=zofia dobrowolska <z.dobrowolska.993@studms.ug.edu.pl>
2026-01-04 20:50:48 +0100

    reorganize styles into folders

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import './styles.scss'
+import './styles/global.scss'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
